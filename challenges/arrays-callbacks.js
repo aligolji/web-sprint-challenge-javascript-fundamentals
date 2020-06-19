4381e26e4f0e6a337bc0b721183cbe084dda9557
unknown <ali-golji@lambdastudents.com>
2020-06-19 14:52:42 -0700

    arrays-callback tasks complete. including stretch.

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -72,6 +72,9 @@ console.log(populationTotal);
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
 
+const consume = function hOf(a, b, cb){
+  return cb(a, b);
+};
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -79,6 +82,17 @@ console.log(populationTotal);
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+const add = function(a, b){
+  return a + b;
+};
+
+const multiply = function(a, b){
+  return a * b;
+};
+
+const greeting = function(firstName, lastName){
+  return `Hello ${firstName} ${lastName}, nice to meet you!`;  
+};
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
 // console.log(consume(2, 2, add)); // 4
@@ -86,11 +100,17 @@ console.log(populationTotal);
 // console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
-
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 /*
-
 Stretch: If you haven't already, convert your array method callbacks into arrow functions.
 
 */
 
+const add = (a, b) => a + b;
+
+const multiply = (a, b) => a * b;
+
+const greeting = (firstName, lastName) => `Hello ${firstName} ${lastName}, nice to meet you!`;
